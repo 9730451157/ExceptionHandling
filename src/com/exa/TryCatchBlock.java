736d5c7277e450ad95646a81c8e5930031ff1687
unknown <pankajtayade369@gmail.com>
2022-01-22 10:40:26 +0530

    Exception

---
+++ src/com/exa/TryCatchBlock.java
@@ -0,0 +1,27 @@
+package com.exa;
+
+public class TryCatchBlock {
+	
+	public void m1() {
+		
+	}
+	
+	public void m2() {
+		
+	}
+	
+	public void m3() {
+		
+		int i = 9/0;
+	}
+
+	public static void main(String[] args) {
+		
+		TryCatchBlock b1=new TryCatchBlock();
+		b1.m3();
+		
+		
+
+	}
+
+}
